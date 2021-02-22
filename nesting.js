4acf47e45ad556fee853f40eacae06d798e78c60
davisengland <davisrodneyengland@gmail.com>
2021-02-22 16:46:33 -0700

    Finished nesting.js

--- nesting.js
+++ nesting.js
@@ -50,7 +50,19 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+function employeeUpdater() {
+  for(let i = 0; i < employees.length; i++) {
+    for(let j = 0; j < employees.length; j++) {
+      if(employees[i].firstName === 'Theo') {
+        employees.splice(i,1)
+      }
+      if(employees[j].firstName === 'Lorie') {
+        employees[j].department = 'HR'
+      }
+  }
+}
+return employees
+}
 
 
 
@@ -68,7 +80,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+function removeDuplicates() {
+  for(let i = 0; i < workplaceAccidents.length; i++) {
+    for(let j = workplaceAccidents.length - 1; j > i; j--) {
+      if(workplaceAccidents[i] === workplaceAccidents[j]) {
+        workplaceAccidents.splice(j,1)
+      }
+    }
+  }
+  return workplaceAccidents
+}
 
 
 
@@ -96,10 +117,8 @@ var cat = {
     2. Assign fluffy2ndFriend the name of Fluffy's 2nd friend.
 */
 
-//Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
-
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 ////////// PROBLEM 4 //////////
@@ -138,8 +157,11 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
-
+function recordCleaner() {
+  for(let i = 0; i < myCar.accidents.length; i++) {
+    myCar.accidents[i].atFaultForAccident = false
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -157,6 +179,15 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
-
-
+function looper() {
+  for(let i = 0; i < numsArr.length; i++) {
+    for(let j = 0; j < numsArr[i].length; j++) {
+      if(numsArr[i][j] % 2 === 0) {
+        numsArr[i].splice(j, 1, 'even')
+      } else {
+        numsArr[i].splice(j, 1, 'odd')
+      }
+    }
+  }
+  return numsArr
+}
